c66fbda791323f392916be510383423d9dafb5aa
Luciana Ferreira da Silva <luciana.silva.1993@gmail.com>
2022-12-20 18:15:50 -0300

    Criacao classe JogoDaVelha

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        JogoDaVelha novoJogo = new JogoDaVelha();
+
+        String posicao;
+
+        int jogadas = 0;
+
         System.out.println("Jogo da Velha");
+
     }
+}
+
+class JogoDaVelha{
+
+    
 }
