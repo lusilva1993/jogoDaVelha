283a4bd5446468e9ea98fd5af742ece055d968ef
Luciana Ferreira da Silva <luciana.silva.1993@gmail.com>
2022-12-20 18:17:34 -0300

    Criada matriz tabuleiro
    Feito criacao do Tabuleiro, usando matriz e switch case para as posicoes

--- src/Main.java
+++ src/Main.java
@@ -17,5 +17,31 @@ public class Main {
 
 class JogoDaVelha{
 
-    
+    String[][] tabuleiro = {{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}};
+
+    public void jogadas(String posicao, String jogador) {
+        switch (posicao) {
+            case "1":
+                tabuleiro[0][0] = jogador;
+            case "2":
+                tabuleiro[0][1] = jogador;
+            case "3":
+                tabuleiro[0][2] = jogador;
+            case "4":
+                tabuleiro[1][0] = jogador;
+            case "5":
+                tabuleiro[1][1] = jogador;
+            case "6":
+                tabuleiro[1][2] = jogador;
+            case "7":
+                tabuleiro[2][0] = jogador;
+            case "8":
+                tabuleiro[2][1] = jogador;
+            case "9":
+                tabuleiro[2][2] = jogador;
+            default:
+                System.out.println("Opção inválida.");
+        }
+    }
+
 }
